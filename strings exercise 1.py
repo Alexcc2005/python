ea92e0f2d293226a41780e7d3dbdc8deb8897a1d
Alex <112864896+Alexcc2005@users.noreply.github.com>
2023-09-19 10:17:23 +0100

    Add files via upload

---
+++ strings exercise 1.py
@@ -0,0 +1,54 @@
+#1
+"""
+print("what is your name?")
+name = input()
+a = len(name)
+print("hello:",name,"the number of letters in your name is:",a)
+"""
+
+#2
+"""
+print("what is your first name?")
+name1 = input()
+print("what is your second name?")
+name2 = input()
+print("Hello",name1,name2+"!")
+"""
+
+#3
+"""
+print("please enter your name in lowercase")
+name1 = input()
+print("please enter your surname in lowercase")
+name2 = input()
+n = name1.upper()
+sn = name2.upper()
+print("HEY",n,sn+"!!!!!")
+"""
+
+#4
+"""
+print("please enter the first line from any nursery rhyme of your choosing")
+nr = input()
+print("nice choice, now choose any 2 numbers")
+num1 = int(input())
+num2 = int(input())
+az = nr[num1:num2]
+print(az)
+"""
+
+#5
+"""
+print("type in any word")
+word = input()
+upword = word.upper()
+"""
+
+#6
+print("please enter your first name")
+fname = input()
+
+print(upword)
+
+
+
